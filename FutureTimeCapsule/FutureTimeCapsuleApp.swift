2c19dea09e3b197a12d144d17b4d595fe13fd98a
Developer <Lepiozdyx@gmail.com>
2026-02-05 14:16:12 -0300

    Initial Commit

---
+++ FutureTimeCapsule/FutureTimeCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureTimeCapsuleApp.swift
+//  FutureTimeCapsule
+//
+//  Created by Alex on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FutureTimeCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
